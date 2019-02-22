f3a9f1074ac393ecacdea99768209ff19b5181c6
jainp7 <47849308+jainp7@users.noreply.github.com>
2019-02-22 15:10:31 +0530

    Update TestRunnerStep.java
    11

--- cucumberdemo/src/test/java/test_runners/TestRunnerStep.java
+++ cucumberdemo/src/test/java/test_runners/TestRunnerStep.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\cucumber_rep\myTestAppSelenium\\cucumberdemo\\src\\test\\java",
+@CucumberOptions(features="C:\\cucumber_rep\\myTestAppSelenium\\cucumberdemo\\src\\test\\java",
                  glue={"step_defs"},
                 		 plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },
                 		 monochrome = true)
